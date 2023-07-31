2d986a865bca1b8327dfe587cba685764f53ac82
Ryan M <ryanmcgurrin3@gmail.com>
2023-07-31 18:44:26 -0400

    Portal block done

--- forge-1.12.2-14.23.5.2859-mdk/src/main/java/com/example/examplemod/blocks/BlockTeleporter.java
+++ forge-1.12.2-14.23.5.2859-mdk/src/main/java/com/example/examplemod/blocks/BlockTeleporter.java
@@ -1,7 +1,9 @@
 package com.example.examplemod.blocks;
 
 import com.example.examplemod.ExampleMod;
+import com.example.examplemod.World.dimension.land.DimensionLand;
 import com.example.examplemod.commands.util.Teleport;
+import com.example.examplemod.init.DimensionInit;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.entity.player.EntityPlayer;
@@ -11,6 +13,8 @@ import net.minecraft.util.EnumHand;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 
+import java.awt.*;
+
 public class BlockTeleporter extends BlockBase{
     public BlockTeleporter(String name, Material material){
         super(name, material);
@@ -19,7 +23,12 @@ public class BlockTeleporter extends BlockBase{
     @Override
     public boolean onBlockActivated(World worldIn, BlockPos pos, IBlockState state, EntityPlayer playerIn, EnumHand hand, EnumFacing facing, float hitX, float hitY, float hitZ) {
         if(!worldIn.isRemote){
-            Teleport.teleportToDimension(playerIn, 2, playerIn.getPosition().getX(),playerIn.getPosition().getY(),playerIn.getPosition().getZ());
+            if(playerIn.dimension == 2){
+                Teleport.teleportToDimension(playerIn, 0, playerIn.getPosition().getX(),playerIn.getPosition().getY(),playerIn.getPosition().getZ());
+            }else{
+                Teleport.teleportToDimension(playerIn, 2, playerIn.getPosition().getX(),playerIn.getPosition().getY(),playerIn.getPosition().getZ());
+            }
+
         }
 
         return false;
